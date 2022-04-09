f77691d9c58aabbc0f4e13f17fedcfbb7bfebad5
Christian Dähn <christian.daehn@gmail.com>
2022-04-09 18:28:20 +0200

    made working again

--- automate.js
+++ automate.js
@@ -1,4 +1,3 @@
-const emailController = require('./email.controller');
 const puppeteer = require('puppeteer');
 
 let browser, selector, errorMessage;
@@ -6,82 +5,66 @@ let browser, selector, errorMessage;
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-// ##
-// # This program visits Redbubble and uploads shirts with the specified data
-// ##
 
-// #########
-// # SETUP #
-// #########
-
-const website = process.argv[2] || "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&dienstleister=327795&anliegen[]=318961&herkunft=1";
-console.log(process.argv[2]);
-// #########
-// # START #
-// #########
+const website = process.argv[2] || "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=121593&dienstleisterlist=122210,122217,122219,122227,122231,122238,122243,122252,122260,122262,122254,122271,122273,122277,122280,122282,122284,327539,122291,122285,122286,122296,150230,122301,122297,122294,122312,122314,122304,122311,122309,122281,122279,122276,122274,122267,122246,122251,122257,122208,122226&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F121593%2F";
 
 function start() {
-  console.log('START');
+    console.log('START');
 
-  (async () => {
+    (async () => {
 
-    browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox']
-    });
-    
-    const page = await browser.newPage();
-    await page.goto(website);
-    
-    // see https://stackoverflow.com/a/47003069/3712591
-    const check = async selector => {
-        // await page.waitFor(selector);??
-        if(await page.$(selector) !== null) {
-            console.log(`${selector} => found`);
-            return selector;
-        } else {
-            console.log(`${selector} => not found`);
-            return false;
+        browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox']
+        });
+
+        const page = await browser.newPage();
+        await page.goto(website);
+
+        const check = async selector => {
+            let element=await page.$(selector)
+            if (element !== null) {
+                return element
+            } else {
+                return false;
+            }
+        };
+
+        const recursion = async function myself() {
+
+            console.log(`trying to find appointment: ${page.url()}`);
+
+            if (await check(".calendar-month-table .buchbar")) {
+
+                console.log("!!!!!! found appointment !!!!!!");
+                finished();
+
+            } else if (await check(".controll .next a")) {
+                let next_page = await check(".controll .next a")
+                let next_page_jshandle=await next_page.getProperty("href")
+                let next_page_uri=next_page_jshandle._remoteObject.value
+                console.log(`no appointment found, checking next page`);
+                await page.goto(next_page_uri)
+                setTimeout(() => myself(), 0);
+
+            } else {
+
+                console.log("end of page, try again! :(");
+                await page.goto(website);
+                setTimeout(() => myself(), 15000);
+
+            }
+
+        };
+
+        recursion();
+
+        function finished() {
+            browser.close();
+            console.log("END");
         }
-    };
 
-    const recursion = async function myself( ) {
-
-        console.log("trying to find appointment!");
-
-        if( await check(".calendar-month-table .buchbar") ) {
-
-            console.log("!!!!!! found appointment !!!!!!");
-            emailController(website);
-            finished();
-        
-        } else if( await check(".collapsible-body a:not(*[title])") ) {
-        
-            console.log("no appointment found, checking next page :(");
-            await page.click( await check(".collapsible-body a:not(*[title])") );
-            setTimeout(() => myself(), 10000);
-        
-        } else {
-        
-            console.log("end of page, try again! :(");
-            await page.goto(website);
-            setTimeout(() => myself(), 15000);
-        
-        }
-
-    };
-
-    recursion();
-
-    // Finished
-    //////////////////////////////////////////////////////////////////////////////
-
-    function finished() {
-        browser.close();
-        console.log("END");
-    }
-
-  })();
+    })();
 
 }
 
